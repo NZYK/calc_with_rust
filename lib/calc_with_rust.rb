ffcf3cb724eaf5e57d07b3bbd19fe4a633063f10
NZYK <nozuyukiwork@gmail.com>
2025-03-19 06:47:47 +0900

    rubyでの実装

--- lib/calc_with_rust.rb
+++ lib/calc_with_rust.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
 
-require_relative "calc_with_rust/version"
+require_relative 'calc_with_rust/version'
 
 module CalcWithRust
   class Error < StandardError; end
-  # Your code goes here...
+
+  module Ruby
+    # 素数探索：エラトステネスの篩
+    def self.primes(n)
+      return [] if n < 2
+
+      # 素数判定用の配列を用意 初期値は全てtrue(素数と仮定)
+      num_arr = Array.new(n + 1, true)
+      # 0と1は素数ではないのでfalseにする
+      num_arr[0] = num_arr[1] = false
+      # ふるいにかける基準値はnの平方根まで。
+      # 例えば　n=100の場合、10を素数と仮定した場合に10 * 10 からふるいにかけるが、このとき最初の値が範囲の最大値となるため。
+      limit = Math.sqrt(n).to_i
+
+      (2..limit).each do |i|
+        next unless num_arr[i]
+
+        # iがtrue(素数)の場合、iの倍数は素数ではないのでfalseにする。
+        # i*2 ~ i*(i-1) まではすでにふるいにかけられているので i*i からスタート
+        (i * i).step(n, i) { |j| num_arr[j] = false }
+      end
+      num_arr.each_index.select { |i| num_arr[i] }
+    end
+  end
 end
--- spec/calc_with_rust_spec.rb
+++ spec/calc_with_rust_spec.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 RSpec.describe CalcWithRust do
-  it "has a version number" do
+  it 'has a version number' do
     expect(CalcWithRust::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'primes' do
+    context 'rubyコードを使うとき' do
+      it '素数が取得できる' do
+        expect(CalcWithRust::Ruby.primes(10)).to eq [2, 3, 5, 7]
+      end
+    end
   end
 end
